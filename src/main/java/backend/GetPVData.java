3338fa21b208c0b56162eda87409cdce7e7abc40
adito0 <aditi.ramalingam@outlook.com>
2020-10-19 18:26:37 +1300

    new

--- src/main/java/backend/GetPVData.java
+++ src/main/java/backend/GetPVData.java
@@ -1,12 +1,11 @@
 package backend;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class GetPVData {
+
     private String SERVER_IP = null;
     private int SERVER_PORT = -1;
 
@@ -26,4 +25,73 @@ public class GetPVData {
         socket.close();
         return serverResponse;
     }
+
+    public String runHeater() throws IOException {
+        Socket socket = new Socket(SERVER_IP, SERVER_PORT);
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        String command = "GHEA";
+        out.println(command);
+        System.out.println("Boo");
+        String serverResponse = input.readLine();
+        System.out.println("Server says: " + serverResponse);
+        socket.close();
+        return serverResponse;
+    }
+
+    public String runSensor() throws IOException {
+        Socket socket = new Socket(SERVER_IP, SERVER_PORT);
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        String command = "GSEN";
+        out.println(command);
+        String serverResponse = input.readLine();
+        System.out.println("Server says: " + serverResponse);
+        socket.close();
+        return serverResponse;
+    }
+
+    public String runHouse() throws IOException {
+        Socket socket = new Socket(SERVER_IP, SERVER_PORT);
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        String command = "GHEC";
+        out.println(command);
+        String serverResponse = input.readLine();
+        System.out.println("Server says: " + serverResponse);
+        socket.close();
+        return serverResponse;
+    }
 }
+/*
+
+            String  str = null;
+            try {
+                System.out.println("Started");
+                str=inFromClient.readLine();
+                System.out.println("From server: " + str);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            str  =  str.split(":")[1].substring(1);
+            try {
+                System.out.println(Float.parseFloat(str));
+                if (Float.parseFloat(str) < housePowerConsumption) {
+                    Socket clientSocket = new Socket("localhost", 6790);
+                    DataOutputStream toServer = new DataOutputStream(clientSocket.getOutputStream());
+                    System.out.println("Grid connected mode ");
+                    toServer.writeBytes("true\n");
+                } else {
+                    Socket clientSocket = new Socket("localhost", 6790);
+                    DataOutputStream toServer = new DataOutputStream(clientSocket.getOutputStream());
+                    System.out.println("Grid disconnected");
+                    toServer.writeBytes("false\n");
+                }
+            }catch (NumberFormatException e){
+                System.out.println("CSW1 state: " +  str);
+            }
+        }
+    }
+}
+*/
--- src/main/java/backend/controller/PowerController.java
+++ src/main/java/backend/controller/PowerController.java
@@ -4,6 +4,7 @@ import backend.GetModel;
 import backend.GetPVData;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,7 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-@Controller
+@RestController
 public class PowerController {
 
     @GetMapping("/data")
@@ -22,7 +23,18 @@ public class PowerController {
 
         try {
             String resp = getPVData.run();
-            map.put("data", resp);
+            String resp1 = getPVData.runHouse();
+            String resp2 = getPVData.runHeater();
+            String resp3 = getPVData.runSensor();
+            boolean isConnected = true;
+            if(Float.parseFloat(resp.substring(33)) > Float.parseFloat(resp1.substring(34))){
+                isConnected = false;
+            }
+            map.put("solar generation", resp);
+            map.put("house consumption", resp1);
+            map.put("heater consumption", resp2);
+            map.put("sensor", resp3);
+            map.put("gridConn", isConnected ? "true" : "false");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -34,21 +46,22 @@ public class PowerController {
     public ModelAndView getIECModel() {
        GetModel getModel = new GetModel("127.0.0.1", 6760);
         String[] modelList = null;
+        String model = "Non connection";
         try {
-            String model = getModel.run();
-            modelList = model.split("\n");
+            model = getModel.run();
+
         } catch (IOException e){
             e.printStackTrace();
         }
-       ModelAndView modelAndView = new ModelAndView("IECdisplay");
-       modelAndView.addObject("LD1", "solarPanel");
-        modelAndView.addObject("LN1", "MMXU");
-        modelAndView.addObject("A1", "TotW: 6\nq: True\nt: 00:59:01");
-        modelAndView.addObject("LD2", "gridInterconnection");
-        modelAndView.addObject("LN2a", "DGEN");
-        modelAndView.addObject("A2a", "mag: 6\nq: True\nt: 00:59:01");
-        modelAndView.addObject("LN2b", "CSWI");
-        modelAndView.addObject("A2b", "cltVal: False\nq: True\nt: 00:59:01");
+       ModelAndView modelAndView = new ModelAndView("display");
+       modelAndView.addObject("LD1", model);
+//        modelAndView.addObject("LN1", "MMXU");
+//        modelAndView.addObject("A1", "TotW: 6\nq: True\nt: 00:59:01");
+//        modelAndView.addObject("LD2", "gridInterconnection");
+//        modelAndView.addObject("LN2a", "DGEN");
+//        modelAndView.addObject("A2a", "mag: 6\nq: True\nt: 00:59:01");
+//        modelAndView.addObject("LN2b", "CSWI");
+//        modelAndView.addObject("A2b", "cltVal: False\nq: True\nt: 00:59:01");
        return modelAndView;
     }
 }
